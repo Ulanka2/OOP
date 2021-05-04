87d59c40a6edeeda533a9f5fdef2606fd14fb811
Ulan <80949499+Ulanka2@users.noreply.github.com>
2021-05-04 19:34:30 +0600

    first commit

---
+++ oop.py
@@ -0,0 +1,22 @@
+# Создаем класс Car
+class lessons:
+
+    # создаем атрибуты класса
+    name = "group1"
+    name2 = "group2"
+    name3 = 'grop3'
+
+    # создаем методы класса
+    def start(self):
+        print ("Osnovy")
+
+    def stop(self):
+        print ("OOP")
+        
+
+
+
+lessons_a = lessons()
+print()
+
+lessons_b = lessons()
